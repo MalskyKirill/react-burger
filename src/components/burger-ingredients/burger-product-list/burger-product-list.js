e2645827b8f031b2c217dd009014294a780f23ea
Kirill Malskiy <sabbat@mail.ru>
2023-11-25 15:41:41 +0300

    правки ревью(обернул вычисления в useMemo)

--- src/components/burger-ingredients/burger-product-list/burger-product-list.js
+++ src/components/burger-ingredients/burger-product-list/burger-product-list.js
@@ -2,12 +2,13 @@ import IngredientList from '../ingredient-list/ingredient-list';
 import styles from './burger-product-list.module.css';
 import PropTypes from 'prop-types';
 import { ingredientPropTypes } from '../../../utils/ingredient-prop-types';
+import { useMemo } from 'react';
 
 const BurgerProductList = ({ data, handleCardClick }) => {
 
-  const dataBun = data.filter((el) => el.type === 'bun');
-  const dataMain = data.filter((el) => el.type === 'main');
-  const dataSauce = data.filter((el) => el.type === 'sauce');
+  const dataBun = useMemo(() => data.filter((el) => el.type === 'bun'), [data]);
+  const dataMain = useMemo(() => data.filter((el) => el.type === 'main'), [data]);
+  const dataSauce = useMemo(() => data.filter((el) => el.type === 'sauce'), [data]);
 
   return (
     <div className={`${styles['burger-product-list']} custom-scroll`}>
