abdd513f874dc64198ba0e2e324bf34ede5431e8
Kirill Malskiy <sabbat@mail.ru>
2024-01-10 16:05:36 +0300

    типизировал компонент NavLinkIcon

--- src/components/layout/nav-link-icon/nav-link-icon.tsx
+++ src/components/layout/nav-link-icon/nav-link-icon.tsx
@@ -1,7 +1,16 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from './nav-link-icon.module.css'
+import styles from './nav-link-icon.module.css';
 
-const NavLinkIcon = ({icon: Icon, children, path, isActive}) => {
+
+type TNavLinkIcon = {
+  icon: React.ElementType,
+  children: string,
+  isActive: boolean,
+  path: string,
+}
+
+const NavLinkIcon = ({icon: Icon, children, path, isActive}: TNavLinkIcon) => {
   return(
     <NavLink to={path} className={`${styles['link-element']} pl-5 pr-5 pt-4 pb-4`}>
       <Icon type={isActive ? 'primary' : 'secondary'}/>
