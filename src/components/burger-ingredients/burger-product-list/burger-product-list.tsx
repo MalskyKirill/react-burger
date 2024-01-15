abaddb02873281fcafad2693ce5d6548ffdb5543
Kirill Malskiy <sabbat@mail.ru>
2024-01-15 13:48:26 +0300

    создал интерфейс IIngredient для типизации данных ингридиента

--- src/components/burger-ingredients/burger-product-list/burger-product-list.tsx
+++ src/components/burger-ingredients/burger-product-list/burger-product-list.tsx
@@ -1,12 +1,11 @@
 import IngredientList from '../ingredient-list/ingredient-list';
 import styles from './burger-product-list.module.css';
-import PropTypes from 'prop-types';
-import { ingredientPropTypes } from '../../../utils/ingredient-prop-types';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { selectAllIngredients } from '../../../services/reducers/ingredients-slice';
+import {IIngredient} from '../../../types/ingredient';
 
-type TBurgerIngredients = {
+type TBurgerProductList = {
   rootRef:  React.RefObject<HTMLDivElement>
   bunRef: React.RefObject<HTMLHeadingElement>;
   sauceRef: React.RefObject<HTMLHeadingElement>;
@@ -20,8 +19,9 @@ const BurgerProductList = ({
   sauceRef,
   mainRef,
   handleScroll,
-}: TBurgerIngredients): JSX.Element => {
-  const ingredients = useSelector(selectAllIngredients);
+}: TBurgerProductList): JSX.Element => {
+
+  const ingredients: Array<IIngredient> = useSelector(selectAllIngredients);
 
   const dataBun = useMemo(
     () => ingredients.filter((el: { type: string }) => el.type === 'bun'),
@@ -47,11 +47,6 @@ const BurgerProductList = ({
       <IngredientList title={'Начинки'} data={dataMain} refItem={mainRef} />
     </div>
   );
-};
-
-BurgerProductList.propTypes = {
-  data: PropTypes.arrayOf(ingredientPropTypes),
-  handleCardClick: PropTypes.func,
 };
 
 export default BurgerProductList;
